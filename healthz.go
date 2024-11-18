036b8eba77f42215617b2a50491fffe3e6d79fe4
christopherhanke <christopherhanke@googlemail.com>
2024-11-18 22:17:50 +0100

    function healthz only serves GET request.

--- healthz.go
+++ healthz.go
@@ -3,6 +3,11 @@ package main
 import "net/http"
 
 func hanlderHealthz(respw http.ResponseWriter, req *http.Request) {
+	if req.Method != "GET" {
+		respw.WriteHeader(405)
+		respw.Write([]byte("not get"))
+		return
+	}
 	respw.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	respw.WriteHeader(200)
 	respw.Write([]byte("OK"))
