89975e0805362be2a83aff071eaa2e46fc7ff5b2
christopherhanke <christopherhanke@googlemail.com>
2024-12-14 21:55:22 +0100

    adding queries for refresh tokens.

---
+++ sql/queries/refresh_tokens.sql
@@ -0,0 +1,15 @@
+-- name: CreateRefreshToken :one
+insert into refresh_tokens (token, created_at, updated_at, user_id, expires_at)
+    values (
+        $1,
+        now(),
+        now(),
+        $2,
+        $3
+)
+returning *;
+
+-- name: GetUserFromRefreshToken :one
+select * from refresh_tokens
+    where token = $1;
+
